5c775bcdea3b290957e3fe31dd3e5b8b1705ec34
thomasPapsterg <thomaspapsterg@gmail.com>
2026-02-01 17:20:22 +0200

    api allages 3

--- api.php
+++ api.php
@@ -10,6 +10,10 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 header('Content-Type: application/json');
+// Προσθήκη για ανάγνωση του Authorization Header σε Apache
+if (!isset($_SERVER['HTTP_AUTHORIZATION']) && isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+    $_SERVER['HTTP_AUTHORIZATION'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+}
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
